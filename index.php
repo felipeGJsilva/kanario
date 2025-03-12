97983b6f2665ed6f32c501ad3aa5892dae9fe6bd
FelipeJansen <132519887+felipeGJsilva@users.noreply.github.com>
2025-03-12 00:12:07 +0000

    feito

---
+++ index.php
@@ -0,0 +1,52 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard inicial</title>
+    <style>
+        body{
+            front-family:helvetica;
+            diskplay:flex;
+            margin:0;
+        }
+        .sidebar{
+            width: 250px;
+            background: #333;
+            color: #fefefe;
+            height: 100vh;
+            padding:20px;
+        }
+        .sidebar ul{
+            list-style: none;
+            padding: 0px;
+        }
+        .sidebar ul li{
+            padding: 10px 0px;
+        }
+        .sidebar ul li a{
+            color: #fefefe;
+            text_decoration:none;
+        }
+        .content{
+            flex-grow:1;
+            padding:20px
+        }
+    </style>
+</head>
+<body>
+    <div class="sidebar">
+        <h2> Dashboard</h2>
+        <ul>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">IRelatórios</a></li>
+            <li><a href="#">Configuração</a></li>
+        </ul>
+    </div>
+    <div class="sidebar">
+        <h2>bem vindo <b> f</b> </h2>
+        <p> este é o painel inicial</p>
+    </div>
+</body>
+</html>
